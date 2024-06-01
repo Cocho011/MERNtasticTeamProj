bd8cf4e88f945a6937cb58c3f7fea2b1d057af89
Crlyhdfrd <152818336+Crlyhdfrd@users.noreply.github.com>
2024-05-31 23:57:05 -0500

    set up data

---
+++ dummyData.js
@@ -0,0 +1,63 @@
+export const users = {
+  98741: {
+    username: "johndoe",
+    password: "password",
+    startDate: "05/13/2024",
+  },
+  56932: {
+    username: "janedoe",
+    password: "password",
+  },
+};
+
+export const userIDs = {
+  johndoe: 98741,
+  janedoe: 56932,
+};
+
+export const Budget = {
+  98741: {
+    "05/13/2024": {
+      totalBudget: "500",
+      spent: "125",
+      spending: [
+        { "05/14", 25, "snacks" },
+        { "05/15", 50, "groceries" },
+        { "05/16", 50, "groceries" }
+      ]
+    },
+
+    "05/06/2024": {
+      totalBudget: "500",
+      spent: "695",
+      spending: [
+        { "05/07", 25, "snacks" },
+        { "05/08", 90, "groceries" },
+        { "05/09", 50, "movies" }
+      ]
+    },
+
+    "04/29/2024": {
+      totalBudget: "500",
+      spent: "90",
+      spending: [
+        { "04/30", 25, "snacks" },
+        { "05/01", 5, "swimming" },
+        { "05/02", 30, "takeout" }
+      ]
+    },
+
+    "04/22/2024": {
+      totalBudget: "500",
+      spent: "700",
+      spending: [
+        { "04/23", 11, "coffee" },
+        { "04/24", 45, "donuts" },
+        { "04/25", 20, "water" }
+      ]
+    },
+
+
+  },
+};
+
