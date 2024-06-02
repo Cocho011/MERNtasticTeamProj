423bb79775026fc1b371a9c1c9bc05e0215279d9
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-06-02 16:11:27 -0500

    built out some typedefs. need to build a custom scalar for date type and need to figure out if im doing the user ids correctly.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,43 @@
 const typeDefs = `
+
+    # TO DO: add custom scalar for Date type
+    # https://stackoverflow.com/questions/49693928/date-and-json-in-type-definition-for-graphql
+
+    type Budget {
+        _id: ID
+        amount: Int
+        # weekDate: Date
+        # not sure which way i should do this?? see Budget.js:
+        # userId: ID
+        # userId: [User]
+    }
+
+    type Spending {
+        amount: Int
+        # timeSubmitted: Date
+        purchaseDescription: String!
+        # userId - same question as on Budget type
+    }
+
     type User {
         _id: ID
         username: String!
         email: String!
+        password: String!
+    }
 
     type Auth {
         token: ID!
         user: User
     }
-}`
+
+    # commented out query until finished
+    # type Query {
+        # stuff goes here. referencing 21 mern -> 05 typedefs-resolvers
+    # }
+
+}'
+`;
 
 
 
