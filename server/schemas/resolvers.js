4df61e5fb6df4954c439e36f2e75308b0b8e89d9
Mariel Miscavage <memiscavage@gmail.com>
2024-06-05 21:01:36 -0500

    undid/erased const resolvers to start fresh...so graphql and apollo dont make me cry

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,55 +6,7 @@ const { useContext } = require('react');
 
 
 const resolvers = {
-    Query: {
-        user: async (parent, args, context) => {
-            if (context.user) {
-                return User.findOne({ _id: context.user._id });
-              }
-              throw AuthenticationError;
-            // TO DO: I think auth goes here?
-         },
-         budget: async (parent, args, context) => {
-            try {
-                const budget = await Budget.findOne({ _id: context.budget._id });
-                return budget;
-            } catch (error) {
-                throw new Error('Failed to fetch budget');
-            }
-        },
-        spending: async (parent, args, context) => {
-            try {
-                const spending = await Spending.find({ _id: context.spending._id });
-                return spending;
-            } catch (error) {
-                throw new Error('Failed to fetch spending');
-            }
-        }
-        },
-    Mutation: {
-        addUser: async (parent, { username, email, password }) => {
-            const user = await User.create({ username, email, password });
-            const token = signToken(user);
-            return { token, user };
-          },
-          login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-      
-            if (!user) {
-              throw AuthenticationError;
-            }
-      
-            const correctPw = await user.isCorrectPassword(password);
-      
-            if (!correctPw) {
-              throw AuthenticationError;
-            }
-      
-            const token = signToken(user);
-      
-            return { token, user };
-          },
-    }
+    
 };
 
 module.exports = resolvers;
