f90fb4ef8ef391b76d506143914e58a3210fa66d
Mariel Miscavage <memiscavage@gmail.com>
2024-06-06 19:02:11 -0500

    rearranged order of query to sync with typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,21 +1,21 @@
-const { useTransform } = require('framer-motion');
+// const { useTransform } = require('framer-motion');
 const { User, Budget, Spending } = require('../models');
-const { useContext } = require('react');
+//const { useContext } = require('react');
 // TO DO: ADD AUTH
 // const { signToken, AuthenticationError } = require('../utils/auth');
 
 
 const resolvers = {
-    Query: {
+    Query: {        
         budgets: async () => {
             return await Budget.find({});
+        },
+        spendings: async () => {
+            return await Spending.find({});
         },
         users: async () => {
             return await User.find({});
         },
-        spendings: async () => {
-            return await Spending.find({});
-        }
     }
 };
 
