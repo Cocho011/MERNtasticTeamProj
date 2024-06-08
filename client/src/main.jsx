12d7f9c887073456c86f81bcc55ab52276ca12c4
Carolina Ochoa <cocho011@fiu.edu>
2024-06-07 21:02:24 -0500

    Made changes to main.jsx, vite.config.js and moved index.html to better location to troubleshoot issues with react

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-const rootElement = document.getElementById('root');
-
-
-rootElement.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
