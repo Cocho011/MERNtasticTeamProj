c5c93167838c461dedc738e9f4df24afa05c315c
Carolina Ochoa <cocho011@fiu.edu>
2024-06-07 20:50:56 -0500

    Deleted package-lock.json to reinstall dependencies correctly

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
 
-root.render(
+
+rootElement.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
