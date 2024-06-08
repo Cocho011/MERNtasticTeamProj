e1ae64cdae773b586a4b16a9d04dd6f0cc28577c
Crlyhdfrd <152818336+Crlyhdfrd@users.noreply.github.com>
2024-06-07 22:02:27 -0500

    dateTime scalar added and tested

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,48 +1,49 @@
 // const { useTransform } = require('framer-motion');
-const { User, Budget, Spending } = require('../models');
+const { User, Budget, Spending } = require("../models");
+const { DateTimeResolver } = require("graphql-scalars");
 //const { useContext } = require('react');
 // TO DO: ADD AUTH
 // const { signToken, AuthenticationError } = require('../utils/auth');
 
-
 const resolvers = {
-    Query: {        
-        budgets: async (parent, args, context) => {
-            // Testing to make sure static data works
-            // try {
-            //     const staticBudgets = [
-            //         { _id: '1', amount: 1000 },
-            //         { _id: '2', amount: 2000 },
-    
-            //     ];
-            //     return staticBudgets;
-            // } catch (error) {
-            //     throw new Error('Failed to fetch budgets');
-            // }
-            try {
-                const budget = await Budget.find({});
-                return budget;
-            } catch (error) {
-                throw new Error('Failed to fetch budget');
-            }
-        },
-        spendings: async (parent, args, context) => {
-            try {
-                const spending = await Spending.find({});
-                return spending;
-            } catch (error) {
-                throw new Error('Failed to fetch spending');
-            }
-        },
-        //This works for querying users
-        users: async () => {
-            return User.find({});
-        },
-        user: async (parent, { userId }) => {
-            return User.findOne({_id: userId});
-            // TO DO: I think auth goes here?
-         }
-    }
+  DateTime: DateTimeResolver,
+  Query: {
+    budgets: async (parent, args, context) => {
+      // Testing to make sure static data works
+      // try {
+      //     const staticBudgets = [
+      //         { _id: '1', amount: 1000 },
+      //         { _id: '2', amount: 2000 },
+
+      //     ];
+      //     return staticBudgets;
+      // } catch (error) {
+      //     throw new Error('Failed to fetch budgets');
+      // }
+      try {
+        const budget = await Budget.find({});
+        return budget;
+      } catch (error) {
+        throw new Error("Failed to fetch budget");
+      }
+    },
+    spendings: async (parent, args, context) => {
+      try {
+        const spending = await Spending.find({});
+        return spending;
+      } catch (error) {
+        throw new Error("Failed to fetch spending");
+      }
+    },
+    //This works for querying users
+    users: async () => {
+      return User.find({});
+    },
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+      // TO DO: I think auth goes here?
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,12 @@ const typeDefs = `
     # TO DO: add custom scalar for Date type
     # https://stackoverflow.com/questions/49693928/date-and-json-in-type-definition-for-graphql
 
+    scalar DateTime
+
     type Budget {
         _id: ID
         amount: Int!
-        # weekDate: Date
+        weekDate: DateTime!
         users: [User]
     }
 
