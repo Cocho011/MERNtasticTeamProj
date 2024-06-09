b11f438419870d7c858cb57f512bb4ef8485f4fa
Carolina Ochoa <cocho011@fiu.edu>
2024-06-09 13:54:35 -0500

    Commit fixes from Meg session

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/');
+connect('mongodb://127.0.0.1:27017/mernstasticdb')
 
 module.exports = connection;
 
