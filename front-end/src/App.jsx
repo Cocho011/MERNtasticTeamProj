3a2ca644ba6e1d0c013d4bfcff3150d32d039c6c
Crlyhdfrd <152818336+Crlyhdfrd@users.noreply.github.com>
2024-06-09 22:06:53 -0500

    changed client to front-end to fix 404 error on npm run dev

---
+++ front-end/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import LoginPage from './pages/LoginPage';
+// import SignupPage from './pages/SignupPage';
+// import DashboardPage from './pages/DashboardPage';
+
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// <Router>
+//   <Routes>
+//     <Route path="/" element={<LoginPage />} />
+//     <Route path="/signup" element={<SignupPage />} />
+//     <Route path="/dashboard" element={<DashboardPage />} />
+//   </Routes>
+// </Router>
