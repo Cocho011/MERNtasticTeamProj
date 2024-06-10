4e263d380044e6682174a7cba07fc169d2f10314
Crlyhdfrd <152818336+Crlyhdfrd@users.noreply.github.com>
2024-06-09 22:26:49 -0500

    front end working without data working

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,23 +1,19 @@
 import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import LoginPage from './pages/LoginPage';
-// import SignupPage from './pages/SignupPage';
-// import DashboardPage from './pages/DashboardPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
-// <Router>
-//   <Routes>
-//     <Route path="/" element={<LoginPage />} />
-//     <Route path="/signup" element={<SignupPage />} />
-//     <Route path="/dashboard" element={<DashboardPage />} />
-//   </Routes>
-// </Router>
