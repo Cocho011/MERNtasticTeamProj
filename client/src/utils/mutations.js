2ad92d52ed9ca12025a7e7edc2f405d53e69bbb5
Mariel Miscavage <memiscavage@gmail.com>
2024-06-09 23:28:52 -0500

    created mutations file added the user and login mutations query from carolinas code

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+export const AddUser= gql`
+mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        password
+      }
+    }
+  }`;
+
+export const Login = gql`
+mutation Login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+      password
+    }
+  }
+}`;
