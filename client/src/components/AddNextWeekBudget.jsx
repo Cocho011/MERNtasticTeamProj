8d3fdc6ae0af6fe0ba68913aedd9bdb4d5063742
Mariel Miscavage <memiscavage@gmail.com>
2024-06-09 23:26:22 -0500

    copied frontend/react code from carolina's branch

--- client/src/components/AddNextWeekBudget.jsx
+++ client/src/components/AddNextWeekBudget.jsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { addNextWeekBudget } from "../../dummyTest/dummyRoutes";
+import { addNextWeekBudget } from "../../../server/seeds/budgetData.json";
 import { useState } from "react";
 
 const SyledAddNextWeekBudget = styled.div`
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,12 +1,94 @@
-function Login({ setLoggedIn }) {
-  return (
-    <div>
-      <h1>Login</h1>
-      <input className="loginUsername" placeholder="username"></input>
-      <input className="loginPasssword" placeholder="password"></input>
-      <button onClick={() => setLoggedIn(true)}>Login</button>
-    </div>
-  );
-}
+  import { useState } from 'react';
+  import { Link } from 'react-router-dom';
+  import { useMutation } from '@apollo/client';
+  import { Login } from '../utils/mutations';
 
-export default Login;
+  import Auth from '../utils/auth';
+
+  const Login = (props) => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(Login);
+
+    // update state based on form input changes
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+
+      setFormState({
+        ...formState,
+        [name]: value,
+      });
+    };
+
+    // submit form
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+      console.log(formState);
+      try {
+        const { data } = await login({
+          variables: { ...formState },
+        });
+
+        Auth.login(data.login.token);
+      } catch (e) {
+        console.error(e);
+      }
+
+      // clear form values
+      setFormState({
+        email: '',
+        password: '',
+      });
+    };
+
+    return (
+      <main className="flex-row justify-center mb-4">
+        <div className="col-12 col-lg-10">
+          <div className="card">
+            <h4 className="card-header bg-dark text-light p-2">Login</h4>
+            <div className="card-body">
+              {data ? (
+                <p>
+                  Success! You may now head{' '}
+                  <Link to="/">back to the homepage.</Link>
+                </p>
+              ) : (
+                <form onSubmit={handleFormSubmit}>
+                  <input
+                    className="form-input"
+                    placeholder="Your email"
+                    name="email"
+                    type="email"
+                    value={formState.email}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className="form-input"
+                    placeholder="******"
+                    name="password"
+                    type="password"
+                    value={formState.password}
+                    onChange={handleChange}
+                  />
+                  <button
+                    className="btn btn-block btn-primary"
+                    style={{ cursor: 'pointer' }}
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                </form>
+              )}
+
+              {error && (
+                <div className="my-3 p-3 bg-danger text-white">
+                  {error.message}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  export default Login;
