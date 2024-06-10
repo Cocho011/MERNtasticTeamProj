ab95ee6c459bf66f5dc3236a930c48c06e6a8392
Carolina Ochoa <cocho011@fiu.edu>
2024-06-09 21:03:38 -0500

    Added the .populate("budgets"in the me query in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,30 +1,17 @@
-// const { useTransform } = require('framer-motion');
 const { User, Budget, Spending } = require('../models');
-//const { useContext } = require('react');
 const { signToken, AuthenticationError } = require('../utils/auth');
-
 
 const resolvers = {
     Query: {
-me: async (parent, args, context) => {
-    if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-        return userData;
-    }  throw AuthenticationError;   
-},  
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).populate("budgets");
+                return userData;
+            }  
+            throw new AuthenticationError('Not authenticated');   
+        },
 
         budgets: async (parent, args, context) => {
-            // Testing to make sure static data works
-            // try {
-            //     const staticBudgets = [
-            //         { _id: '1', amount: 1000 },
-            //         { _id: '2', amount: 2000 },
-
-            //     ];
-            //     return staticBudgets;
-            // } catch (error) {
-            //     throw new Error('Failed to fetch budgets');
-            // }
             try {
                 const budget = await Budget.find({});
                 return budget;
@@ -40,14 +27,9 @@ me: async (parent, args, context) => {
                 throw new Error('Failed to fetch spending');
             }
         },
-        //This works for querying users
         users: async () => {
             return User.find({});
         },
-        // user: async (parent, { userId }) => {
-        //     return User.findOne({_id: userId});
-        //     // TO DO: I think auth goes here?
-        //  },
     },
 
     Mutation: {
@@ -55,33 +37,36 @@ me: async (parent, args, context) => {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
-          },
-          login: async (parent, { email, password }) => {
+        },
+        login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
-      
+
             if (!user) {
-              throw AuthenticationError;
+                throw new AuthenticationError('Incorrect credentials');
             }
-      
+
             const correctPw = await user.isCorrectPassword(password);
-      
+
             if (!correctPw) {
-              throw AuthenticationError;
+                throw new AuthenticationError('Incorrect credentials');
             }
-      
+
             const token = signToken(user);
-      
+
             return { token, user };
-          },
+        },
       
-          // full weekly budget 
-        addBudget: async (parent, { amount, weekDate,}, context) => {
+        addBudget: async (parent, { amount, weekDate }, context) => {
             if (context.user) {
-                const budget= await Budget.create({ amount, weekDate, });
-            const user = await User.findByIdAndUpdate(context.user._id, { $push: { budgets: budget._id } }, { new: true });
-            return user;    
-        }
-        throw AuthenticationError;
+                const budget = await Budget.create({ amount, weekDate });
+                const user = await User.findByIdAndUpdate(
+                    context.user._id, 
+                    { $push: { budgets: budget._id } }, 
+                    { new: true }
+                ).populate("budgets");
+                return user;    
+            }
+            throw new AuthenticationError('Not authenticated');
         },
 
         addSpending: async (parent, { amount, timeSubmitted, purchaseDescription, userId }) => {
